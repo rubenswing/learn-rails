d70857dd77afbec1e525b9bef2d28ce80ca6d202
Ruben Swing <rubenswing@gmail.com>
2014-03-03 00:05:32 +0530

    dynamic home page

---
+++ app/models/owner.rb
@@ -0,0 +1,24 @@
+require 'date'
+require 'time'
+
+class Owner
+
+  def name
+    name = 'Ruben Swing'
+  end
+
+  def birthdate
+    birthdate = Date.new(1976,06,18)
+  end
+
+  def countdown
+    today = Date.today
+    birthday = Date.new(today.year, birthdate.month, birthdate.day)
+    if birthday > today
+      countdown = (birthday - today).to_i
+    else
+      countdown = (birthday.next_year - today).to_i
+    end
+  end
+
+end
